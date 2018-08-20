a97fc32d78df30eeb3215b10776736c96291e94c
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-20 11:59:25 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,21 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+
 end
 
 def add_element_to_end_of_array(array, element)
-  
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
@@ -23,13 +23,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
