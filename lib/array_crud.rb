062eb1cffec7410dad21467910944e48d98c899a
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-20 12:01:41 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  [Hoots, Mob, Panda, Sable]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
